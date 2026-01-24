ec4386ed90b417b49b1852b6170b175b5f10644b
Aid Arslanagic <aarsla@gmail.com>
2026-01-24 17:48:38 +0100

    Add sitemap.xml generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://wooplugin.pro',
   output: 'static',
   adapter: vercel(),
 
@@ -17,5 +19,5 @@ export default defineConfig({
     }
   },
 
-  integrations: [react()]
+  integrations: [react(), sitemap()]
 });
