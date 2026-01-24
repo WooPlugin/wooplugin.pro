42b6512f234a15740dea2c27f3dcd6c5ca5e9d6e
Aid Arslanagic <aarsla@gmail.com>
2026-01-24 23:17:12 +0100

    Update docs with Pro installation flow and API details

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import vercel from '@astrojs/vercel';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://wooplugin.pro',
-  output: 'hybrid',
+  output: 'static',
   adapter: vercel(),
   build: {
     inlineStylesheets: 'always'
